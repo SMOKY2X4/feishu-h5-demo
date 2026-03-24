0d3e12fc484af6d4568ab3b76bf9a9ff4cafdb69
SMOKY2X4 <smoky2x4@gmail.com>
2026-03-25 00:30:26 +0800

    fix: cloudflare function route

--- functions/api/create-doc.js
+++ functions/api/create-doc.js
@@ -1,1 +1,14 @@
-export { onRequest } from '../../server/index.js'
+export async function onRequest(context) {
+  return new Response(
+    JSON.stringify({
+      ok: true,
+      title: 'Cloudflare Functions 测试文档',
+      url: 'https://example.com/doc/cloudflare',
+    }),
+    {
+      headers: {
+        'content-type': 'application/json; charset=UTF-8',
+      },
+    },
+  )
+}
